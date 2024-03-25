a82ed01027b0dc3a19bc06ca57f784f0ffe1485d
Andrew <huli-gan_94@mail.ru>
2024-03-25 14:48:17 +0300

    Получение классов

--- src/js/index.js
+++ src/js/index.js
@@ -31,3 +31,32 @@ function shuffleAndPickRandom(items) {
 const flipCard = (card) => {
   console.log("родитель карточки получен", card);
 };
+/**
+ * Состояние игры
+ * @property {boolean} isGameStarted - Игра началась или нет.
+ * @property {number} totalTime - Общее время в игре.
+ * @property {number} flippedCards - Количество перевернутых карточек.
+ * @property {number} totalFlips - Общее количество перевернутых карточек.
+ */
+const STATE = {
+  isGameStarted: false,
+  totalTime: 0,
+  flippedCards: 0,
+  totalFlips: 0,
+};
+
+/**
+ * Контролы игры
+ * @property {HTMLDivElement} boardContainer - Контейнер игрового поля.
+ * @property {HTMLDivElement} board - Основное содержимое поля (4x4).
+ * @property {HTMLDivElement} moves - Контрол для учета шагов.
+ * @property {HTMLDivElement} timer - Контрол для учета времени.
+ * @property {HTMLButtonElement} start - Кнопка для старта игры.
+ */
+const SELECTORS = {
+  boardContainer: document.querySelector(".board-container"),
+  board: document.querySelector(".board"),
+  moves: document.querySelector(".moves"),
+  timer: document.querySelector(".timer"),
+  start: document.querySelector("button"),
+};
